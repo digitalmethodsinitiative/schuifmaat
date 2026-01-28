9df96339d58e3fa86deb111eff90dd3413e5a2dc
stijn-uva <stijn.peeters@uva.nl>
2026-01-28 16:54:26 +0100

    Rework scrolling and waiting logic to be more efficient

--- content.js
+++ content.js
@@ -1,3 +1,9 @@
+const SCROLL_SPEED = 6000;
+
+let last_seen_post_id;
+let loops_since_retry = 0;
+let testing_retry = false;
+
 async function init() {
     const running_tab_id = await get_prop('scrolling');
     const own_tab_id = await get_tab_id();
@@ -12,66 +18,101 @@ async function init() {
     await scroll_page();
 }
 
-let last_seen_post_id;
-let loops_since_retry = 0;
-let testing_retry = false;
+/**
+ * Reload the page to resume scrolling where we left off
+ *
+ * Updates the 'max_id' query param to request all tweets older than the last
+ * one seen and reloads the page. Useful to avoid memory leaks/the page
+ * consuming too many resources. Note that this also re-inits the content
+ * script!
+ *
+ * @returns {Promise<void>}
+ */
+async function refresh() {
+    let new_url;
+    // make new URL with max_id parameter
+    // set it to the last seen post minus one, i.e. anything older
+    // than the last seen post
+    if (last_seen_post_id) {
+        new_url = window.location.href;
+        const new_max_id = parseInt(last_seen_post_id) - 1;
+        const old_q = new_url.match(/q=([^&]+)&/)[1];
+        let q = decodeURIComponent(old_q);
+        if (q.indexOf('max_id:') >= 0) {
+            // update max_id already in query
+            q = q.replace(/max_id:[0-9]+/g, 'max_id:' + new_max_id);
+        } else {
+            // no max_id parameter yet
+            q = q + ' max_id:' + new_max_id;
+        }
+        q = encodeURIComponent(q);
+        new_url = new_url.replace(old_q, q);
+    } else {
+        // if we've not seen any posts yet, just refresh the page
+        new_url = window.location.href;
+    }
+    window.location.href = new_url;
+}
 
-async function scroll_page() {
-    let is_running = await get_prop('scrolling');
-
-    // check if we're waiting for the rate limit to clear
-    // if so, wait (and periodically log an update)
+/**
+ * Wait until timeout expires
+ *
+ * When awaited, will block until the timeout expires. If there is no timeout,
+ * return immediately.
+ *
+ * @returns {Promise<void>}
+ */
+async function blocking_timeout() {
     const timeout = await get_prop('timeout');
+    if(!timeout) {
+        return;
+    }
+
     let i = 0;
     while(timeout > Date.now()) {
         await wait(500);
         if(i % 120 === 0) {
+            // update every 60 seconds (120 * 500)
             const seconds = Math.round((timeout - Date.now()) / 1000);
-            write_log(`Waiting ${seconds} seconds...`);
+            write_log(`Waiting ${seconds} seconds for rate limit reset...`);
         }
         i += 1;
     }
 
-    if(timeout) {
-        write_log('Resuming scroll');
-    }
+    write_log('Resuming scroll');
     await set_prop('timeout', false);
+}
+
+/**
+ * Main loop
+ *
+ * @returns {Promise<void>}
+ */
+let first_loop = true;
+async function scroll_page() {
+    let is_running = await get_prop('scrolling');
 
     // main loop - scroll until we can no longer scroll, then do something clever
     while (is_running) {
         is_running = await get_prop('scrolling');
 
+        // check if we're waiting for the rate limit to clear
+        // if so, wait (and periodically log an update)
+        if(!first_loop) {
+            // always scroll at least once before waiting, in case we
+            // immediately get results after refreshing
+            await blocking_timeout();
+        }
+
         // are we testing if a retry makes new posts appear?
         if (testing_retry) {
             if (loops_since_retry > 1) {
                 // wait and refresh
-                write_log('No new posts after retrying. Waiting 5 minutes and refreshing before checking again.');
-                let new_url;
-                // make new URL with max_id parameter
-                // set it to the last seen post minus one, i.e. anything older
-                // than the last seen post
-                if (last_seen_post_id) {
-                    new_url = window.location.href;
-                    const new_max_id = parseInt(last_seen_post_id) - 1;
-                    const old_q = new_url.match(/q=([^&]+)&/)[1];
-                    let q = decodeURIComponent(old_q);
-                    if (q.indexOf('max_id:') >= 0) {
-                        // update max_id already in query
-                        q = q.replace(/max_id:[0-9]+/g, 'max_id:' + new_max_id);
-                    } else {
-                        // no max_id parameter yet
-                        q = q + ' max_id:' + new_max_id;
-                    }
-                    q = encodeURIComponent(q);
-                    new_url = new_url.replace(old_q, q);
-                } else {
-                    // if we've not seen any posts yet, just refresh the page
-                    new_url = window.location.href;
-                }
+                write_log('No new posts after retrying. Refreshing before checking again.');
                 await set_prop('timeout', Date.now() + (5 * 60 * 1000));
-                window.location.href = new_url;
-                testing_retry = false;
-                loops_since_retry = 0;
+                // this reloads the page and the content script, so nothing
+                // after this line will be executed!
+                await refresh();
             } else {
                 loops_since_retry += 1
             }
@@ -87,6 +128,7 @@ async function scroll_page() {
         if (have_went_wrong && retry_button) {
             write_log("Got an error, clicking 'Retry...'");
             retry_button[0].click();
+            // wait a bit to allow new posts to be loaded
             await wait(2000);
             await scroll_to_bottom();
         }
@@ -101,17 +143,20 @@ async function scroll_page() {
         }
 
         // if we have no new tweets, start counting down until we consider ourselves rate-limited
-        if (last_seen_post_id === previous_last_post_id) {
+        if (!last_seen_post_id || last_seen_post_id === previous_last_post_id) {
             write_log(`No new posts detected in this loop (${loops_since_retry + 1})`);
             if (!testing_retry) {
                 testing_retry = true;
             }
+        } else {
+            // reset timeout, since we have data again
+            loops_since_retry = 0;
+            await set_prop('timeout', false);
         }
-        await wait(1000); // Small pause between iterations to avoid hammering X.com
+        first_loop = false;
     }
 
     write_log('Stop requested, ending loop.');
-    browser.runtime.sendMessage({done: true}); // Signal that the process is done
     set_prop('scrolling', false);
 }
 
@@ -134,7 +179,7 @@ async function scroll_to_bottom() {
     // need to be at the bottom this long to consider ourselves at the end
     const grace_period = 1000;
     const direction = 1; //down
-    const scrollspeed = get_pixel_speed(3500);
+    const scrollspeed = get_pixel_speed(SCROLL_SPEED);
     const deltaPixel = scrollspeed[0];
     const deltaTime = scrollspeed[1];
     const reqAnimDeltaPixel = Math.round(1000.0 / deltaTime * deltaPixel);
